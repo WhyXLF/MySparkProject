34a0bc9953f412dc2cca7a8a8316cd2bb184cf9b
xiaoliufu <xiaoliufu@meituan.com>
2018-04-15 21:10:28 +0800

    add hiveContext

---
+++ src/main/scala/com/xlf/spark/HiveContextApp.scala
@@ -0,0 +1,27 @@
+package com.xlf.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.hive.HiveContext
+
+/**
+  * hiv
+  */
+object HiveContextApp {
+  def main(args: Array[String]): Unit = {
+    val path = args(0)
+    //创建响应的Context
+    val sparkConf = new SparkConf()
+    //在测试或者生产中，AppName和Master我们是通过脚本进行制定
+//    sparkConf.setAppName("SQLContextApp").setMaster("local[2]")
+
+    val sc = new SparkContext(sparkConf)
+    val hiveContext = new HiveContext(sc)
+
+    //相关处理
+    hiveContext.table("emp").show()
+
+    //关闭资源
+    sc.stop()
+  }
+}
